060b54b02933f2cecbb67f4b576afeddc4b3792f
KevinMidboe <kevin.midboe@gmail.com>
2017-12-22 11:35:45 +0100

    Added a setup.py for easy install and execution of script.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import torrentSearch
+
+setup(
+    name='torrentSearch',
+    version=torrentSearch.__version__,
+    packages=find_packages(),
+    author='KevinMidboe',
+    description='Search For Torrents',
+    long_description="README on github : https://github.com/KevinMidboe/torrent_search",
+    install_requires=[
+        'docopt',
+        'beautifulsoup4',
+        'termcolor',
+    ],
+    url='https://github.com/KevinMidboe/torrent_search',
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+        'console_scripts': [
+            'search = torrentSearch.search:main',
+        ],
+    },
+)
